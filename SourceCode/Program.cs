78b04e47cd4292a5ff09dc95098479ed440ff70b
Nick <112820233+Wraith0011@users.noreply.github.com>
2024-10-16 18:50:56 -0500

    Add files via upload

--- SourceCode/Program.cs
+++ SourceCode/Program.cs
@@ -1,2 +1,11 @@
-﻿using var game = new PixelBox.GameCore();
-game.Run();
+﻿using System;
+using System.Diagnostics;
+
+using var game = new PixelBox.GameCore();
+try
+{
+    game.Run();
+} catch (Exception exception)
+{
+    Debug.WriteLine(exception);
+}
